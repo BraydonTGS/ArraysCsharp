051af083f606064ee8eb27fe936b301092efdaa2
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-09-19 15:18:17 -0500

    array practice

--- ArraysCSharp/Program.cs
+++ ArraysCSharp/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
 
+Title = "Array Practice";
+
+WriteLine("==> Array Practice <==");
+// Arrays: 0 Based, Fixed Type, Fixed Size //
+
+// Scores // 
+int[] scores = new int[3];
+scores[0] = 10;
+scores[1] = 12;
+scores[2] = 11;
+
+foreach (var item in scores)
+{
+    WriteLine($"\nThe score is {item}");
+
+}
+WriteLine($"\nThere are {scores.Length} items");
+
+// Inventory // 
+string[] inventory = { "potion", "stick", "ring", "shield", "torch" };
+
+for (int i = 0; i < inventory.Length; i++)
+{
+    WriteLine($"\n{inventory[i]}");
+}
+
+bool[] answers = { true, true, false };
+
+// Monsters[] monster = new Monster[5]; //
+
+WriteLine("\nPress any key to exit");
+
+ReadKey();
