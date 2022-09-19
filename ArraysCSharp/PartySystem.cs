765e53600a71ae12d9f3b11cafb42970d96920bd
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-09-19 15:58:50 -0500

    finished the party system class

--- ArraysCSharp/PartySystem.cs
+++ ArraysCSharp/PartySystem.cs
@@ -5,13 +5,20 @@ namespace ArraysCSharp
     public class PartySystem
     {
 
-        Character[] MyTeam = new Character[3];
+        Character[] MyTeam;
         public PartySystem()
         {
             // Construct the team //
-            MyTeam[0] = new Character("Pikachu", "Electirc", ConsoleColor.Yellow, 10);
-            MyTeam[1] = new Character("Charizard", "Fire", ConsoleColor.Red, 40);
-            MyTeam[2] = new Character("Bulbasaur", "Grass", ConsoleColor.Green, 12);
+            //MyTeam[0] = new Character("Pikachu", "Electirc", ConsoleColor.Yellow, 10);
+            //MyTeam[1] = new Character("Charizard", "Fire", ConsoleColor.Red, 40);
+            //MyTeam[2] = new Character("Bulbasaur", "Grass", ConsoleColor.Green, 12);
+
+            // Initializer Syntax // 
+            MyTeam = new Character[3]{
+                new Character("Pikachu", "Electirc", ConsoleColor.Yellow, 10),
+                new Character("Charizard", "Fire", ConsoleColor.Red, 40),
+                new Character("Bulbasaur", "Grass", ConsoleColor.Green, 12)
+        };
         }
 
         public void Run()
